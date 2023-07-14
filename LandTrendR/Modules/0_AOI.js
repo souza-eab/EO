45ebbbfe2a603ca8201de1c06cc22ad472f93038
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-07-14 10:53:56 -0300

    Create 0_AOI.js

---
+++ LandTrendR/Modules/0_AOI.js
@@ -0,0 +1,24 @@
+// AOI 
+var pontos = ee.FeatureCollection('users/felipelenti/20230711_044903_loss_and_sec_veg');
+
+// Imprimir as coordenadas de cada AOI
+var imprimirCoordenadas = function(feature) {
+  var ponto = ee.Feature(feature);
+  var coordenadas = ponto.geometry().coordinates().getInfo();
+  print('Coordenadas do Ponto:', coordenadas);
+};
+
+// Lista AOI
+var listaPontos = pontos.toList(pontos.size());
+
+// Itera sobre a lista de pontos e imprime as coordenadas
+listaPontos.evaluate(function(pontos) {
+  pontos.forEach(imprimirCoordenadas);
+});
+
+/// Output
+// AOI |> Formigueiro : [-44.1285,-11.0351]);
+// AOI: [-44.263093028651596,-11.141555021980862]
+// AOI: [-47.211234608351944,-15.851597404114354]
+// AOI: [-44.48368022273305,-7.947553865379715]
+// AOI: [-44.9203867657018,-8.078102643933084]
