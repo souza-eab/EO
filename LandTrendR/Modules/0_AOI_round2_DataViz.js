11f5c34a2112a20d00530d63c6d24bee6888bbf7
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-09-04 11:24:59 -0300

    Update 0_AOI_round2_DataViz.js

--- LandTrendR/Modules/0_AOI_round2_DataViz.js
+++ LandTrendR/Modules/0_AOI_round2_DataViz.js
@@ -92,3 +92,66 @@ var cerrado = ee.FeatureCollection("projects/mapbiomas-workspace/AUXILIAR/biomas
 Map.addLayer(cerrado, {color: 'gray', opacity: 0.3}, 'Biome-Cerrado')
 Map.centerObject(cerrado);                 
 Map.setZoom(5.5)
+
+
+
+// Define a Point object.
+var AOI_round2 = ee.Geometry.Point(-47.100270247986415,-17.065932751168447);
+
+// Apply the buffer method to the Point object.
+var pointBuffer = AOI_round2.buffer({'distance': 1000});
+
+// Print the result to the console.
+print('point.buffer(...) =', pointBuffer);
+
+// Display relevant geometries on the map.
+
+Map.centerObject(AOI_round2, 16);
+Map.addLayer(AOI_round2,
+             {'color': 'black'},
+             'Geometry [black]: point');
+Map.addLayer(pointBuffer,
+             {'color': 'red'},
+             'Result [red]: point.buffer');
+             
+             
+var AOI = /* color: #d63000 */ee.List(
+    [[1.3634359574461996, 31.636034304440155],
+     [3.6046468949461996, 31.148372138748694],
+     [-0.9656656050538004, 29.172742327143503],
+     [2.8575765824461996, 29.172742327143503],
+     [0.7042562699461996, 30.506858885801098],
+     [-1.6248452925538004, 30.884738935660458],
+     [-3.8221109175538004, 27.62655429384725],
+     [2.2862875199461996, 25.781544169671214]]);
+
+var AOI =ee.List([[-49.12170662670784,-15.809106234938541]  // Santo Antônio, Pirenópolis - GO, Brasil
+, [-48.88653106956142,-15.644331240083975] // Lagolandia; 
+,[-47.100270247986415,-17.065932751168447] // Porto Velho
+,[-46.92517564349423,-17.114172717153416]
+,[-45.266480925804025,-11.829634919925066]
+,[-45.39937799093191,-11.925875599319191]
+,[-54.61543339408989,-16.00990782327285]
+,[-54.61114185966606,-16.033006951551222]
+,[-48.08248542732097,-7.657674076243871]
+,[-48.04377578681804,-7.648657065174058]
+,[-48.1090929407487,-7.67247517123241]
+, [-52.036927437404685,-18.89070712606298]
+, [-51.90687420432634,-18.86462180175331]
+, [-46.9769624927473,-19.178707714642652]
+, [-46.48942911803145,-19.116632942516436]
+, [-45.50378225900385,-13.550152678041423]
+, [-45.64454458810541,-13.509429688883838]
+, [-44.32547470148165,-11.038594004898945]
+, [-44.15105929247334,-11.033412526563287]
+, [-45.081314362784795,-11.379133298351128]])  
+
+
+// var mp = ee.FeatureCollection(AOI)
+// print(mp)
+var mp = ee.FeatureCollection(AOI.map(function(p){
+  var point = ee.Feature(ee.Geometry.Point(p), {})
+  return point
+}))
+Map.addLayer(mp, {'color': 'red'}, 'AOI')
+print(mp)
